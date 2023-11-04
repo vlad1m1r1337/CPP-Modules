83d7f51747322ec917bd41cda34a8d8ad6c4732d
vlad1m1r1337 <vladim1r.gribkov@yandex.ru>
2023-11-04 11:21:14 +0400

    rdy ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,17 +1,15 @@
-#include "iomanip"
 #include "iostream"
 
 int main() {
     std::string string = "HI THIS IS BRAIN";
-//    std::string* stringPTR = &string;
+    std::string* stringPTR = &string;
     std::string& stringREF = string;
 
-//    std::cout << &string << std::endl;
-//    std::cout << stringPTR << std::endl;
-//    std::cout << &stringREF << std::endl << std::endl;
-//
-//    std::cout << string << std::endl;
-//    std::cout << *stringPTR << std::endl;
-//    std::cout << stringREF << std::endl;
+    std::cout << &string << std::endl;
+    std::cout << stringPTR << std::endl;
+    std::cout << &stringREF << std::endl << std::endl;
 
+    std::cout << string << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
 }
