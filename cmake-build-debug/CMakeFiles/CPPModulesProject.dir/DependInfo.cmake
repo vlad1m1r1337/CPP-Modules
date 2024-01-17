6d4a78840737c8341113f00098b09747ea10ce32
lo <vladim1r.gribkov@yandex.ru>
2024-01-17 20:59:19 +0400

    start cpp05

---
+++ cmake-build-debug/CMakeFiles/CPPModulesProject.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/AMateria.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/AMateria.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/AMateria.cpp.o.d"
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/Character.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Character.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Character.cpp.o.d"
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/Cure.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Cure.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Cure.cpp.o.d"
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/ICharacter.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/ICharacter.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/ICharacter.cpp.o.d"
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/Ice.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Ice.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Ice.cpp.o.d"
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/MateriaSource.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/MateriaSource.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/MateriaSource.cpp.o.d"
+  "/Users/vgribkov/Desktop/CPP__Modules/CPP04/ex03/main.cpp" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/main.cpp.o" "gcc" "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CPPModulesProject.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/AMateria.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/AMateria.cpp.o.d"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Character.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Character.cpp.o.d"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Cure.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Cure.cpp.o.d"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/ICharacter.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/ICharacter.cpp.o.d"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Ice.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/Ice.cpp.o.d"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/MateriaSource.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/MateriaSource.cpp.o.d"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/main.cpp.o"
+  "CMakeFiles/CPPModulesProject.dir/CPP04/ex03/main.cpp.o.d"
+  "CPPModulesProject"
+  "CPPModulesProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPModulesProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
